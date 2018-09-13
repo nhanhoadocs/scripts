bf3825c528f076cb5b841fc404e8a19c4ad6fae4
datpt <thanhdatkma96@gmail.com>
2018-09-13 14:30:27 +0700

    update late

--- ELK/filebeat-kafka.sh
+++ ELK/filebeat-kafka.sh
@@ -28,7 +28,7 @@ systemctl restart rsyslog
 
 ########### Config Log CMD ##############
 
-cat scripts/ELK/envcmd.txt >> ~/.bash_profile
+cat ~/scripts/ELK/envcmd.txt >> ~/.bash_profile
 
 source ~/.bash_profile
 
