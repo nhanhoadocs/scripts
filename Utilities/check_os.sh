3023139a64f8f02f84b9b5019e080cfc31f7689e
Dang Xuan Canh <canhdx@nhanhoa.com.vn>
2018-12-12 19:08:44 +0700

    Update cmdlog_script

---
+++ Utilities/check_os.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# Check OS, Version, Bit and do somethings
+# CanhDX NhanHoa Cloud Team 
+
+
+if [ -f /etc/os-release ]; then
+    # freedesktop and systemd
+    . /etc/os-release
+    OS=$NAME
+    VER=$VERSION_ID
+elif type lsb_release >/dev/null 2>&1; then
+    # linuxbase
+    OS=$(lsb_release -si)
+    VER=$(lsb_release -sr)
+elif [ -f /etc/lsb-release ]; then
+    # Debian/Ubuntu without lsb_release
+    . /etc/lsb-release
+    OS=$DISTRIB_ID
+    VER=$DISTRIB_RELEASE
+elif [ -f /etc/debian_version ]; then
+    # Older Debian/Ubuntu/etc.
+    OS=Debian
+    VER=$(cat /etc/debian_version)
+elif [ -f /etc/SuSe-release ]; then
+    # Older SuSE/etc.
+    ...
+elif [ -f /etc/redhat-release ]; then
+    # Older Red Hat, CentOS, etc.
+    ...
+else
+    # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+    OS=$(uname -s)
+    VER=$(uname -r)
+fi
+
+case $(uname -m) in
+x86_64)
+    BITS=64
+    ;;
+i*86)
+    BITS=32
+    ;;
+*)
+    BITS=?
+    ;;
+esac
+
+echo $OS
+echo $VER
+echo $BITS
